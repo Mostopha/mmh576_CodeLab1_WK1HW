6b24316b561363458c9b23ffb37e9ea16b5e91fd
Mostafa Haque <93haque@gmail.com>
2017-02-01 14:23:26 -0500

    Uploaded correct game

---
+++ Code Lab 1 Homework 1/Assets/Scripts/destroySelf.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroySelf : MonoBehaviour {
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+
+        Invoke("Destroy", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Destroy()
+    {
+        Destroy(bullet);
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        Invoke("Destory", 0.5f);
+    }
+}
