367628ac6a2eeca2cd6ebc13e908b40b6bfa8f3a
Mostafa Haque <93haque@gmail.com>
2017-02-01 14:17:25 -0500

    Code Lab 1 Homework Uploaded!

---
+++ Code Lab 1 Game 1/Assets/scripts/GoalMoveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalMoveScript : MonoBehaviour {
+
+    public float moveMod;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector3
+            (
+            
+             moveMod*( Mathf.PerlinNoise(Time.timeSinceLevelLoad, 0)-0.5f),
+             moveMod*(Mathf.PerlinNoise(0, Time.timeSinceLevelLoad)-0.5f),
+             
+             0));
+
+
+	}
+}
